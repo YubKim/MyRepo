fe1ab2fa0bef6774d35ed3b9d1cb807e810237e4
YubKim <InyubKim@gim-in-yeob-ui-MacBook-Pro.local>
2016-10-12 21:23:36 +0900

    New Git Test
    Creation new Git Test file

---
+++ git_test/git_test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  git_test
+//
+//  Created by 김인엽 on 2016. 10. 12..
+//  Copyright © 2016년 iykim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
